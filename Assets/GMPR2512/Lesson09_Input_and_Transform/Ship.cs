fa1a0cfdeb0e928efe71e7f2f205f5c94d63214b
Ralph200607 <rjlinding@gmail.com>
2025-10-21 10:23:32 -0600

    Continuing Ship, Projectile object/prefab

--- Assets/GMPR2512/Lesson09_Input_and_Transform/Ship.cs
+++ Assets/GMPR2512/Lesson09_Input_and_Transform/Ship.cs
@@ -1,35 +1,143 @@
 using UnityEngine;
-using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 
-public class Ship : MonoBehaviour
+namespace Lesson09_Input_and_Transform
 {
-    private InputAction _moveAction, _spinAction;
-
-
-    [SerializeField] float _movementSpeed, _spinSpeed;
-
-    void Awake()
+    public class Ship : MonoBehaviour
     {
-        //this creates an input method that is decoupled from the input device
-        _moveAction = InputSystem.actions.FindAction("Ship/Move");
-        _spinAction = InputSystem.actions.FindAction("Ship/Rotate");
-        Debug.Log(_moveAction);
-    }
-    void Update()
-    {
-        #region Movement
-        Vector2 moveDirection = _moveAction.ReadValue<Vector2>();
-        Debug.Log(moveDirection);
+        private InputAction _moveAction, _spinAction, _fireAction;
 
-        Vector2 moveVelocity = moveDirection * _movementSpeed * Time.deltaTime;
+        [SerializeField] private float _movementSpeed = 5, _spinSpeed = 500, _projectileSpeed = 10;
+        [SerializeField] private float _minRotation = 25, _maxRotation = -25;
+        [SerializeField] private GameObject _projectilePrefab;
 
-        transform.Translate(moveVelocity, Space.World);
+        
+
+        void Awake()
+        {
+            //this creates an input method that is decoupled from the input device
+            _moveAction = InputSystem.actions.FindAction("Ship/Move");
+            _spinAction = InputSystem.actions.FindAction("Ship/Rotate");
+            _fireAction = InputSystem.actions.FindAction("Ship/Fire");
+        }
+
+        #region input disable/enable
+        //Unity will keep the input actions disabled by default
+        //for efficiency reasons. So, we need to enable/disable them.
+        //It's best practice to include the methods below.
+        void OnEnable()
+        {
+            #region enable everything
+            if (_moveAction != null)
+            {
+                _moveAction.Enable();
+            }
+            if (_spinAction != null)
+            {
+                _spinAction.Enable();
+            }
+            if (_fireAction != null)
+            {
+                _fireAction.Enable();
+            }
+            #endregion
+            //register methods with the fire action
+            _fireAction.performed += FireButtonPressed;
+            _fireAction.canceled += FireButtonReleased;
+
+        }
+        void OnDisable()
+        {
+            if (_moveAction != null)
+            {
+                _moveAction.Disable();
+            }
+            if (_spinAction != null)
+            {
+                _spinAction.Disable();
+            }
+            if(_fireAction != null)
+            {
+                _fireAction.Disable();
+            }
+        }
         #endregion
-        //Exercise: give the player the ability to spin the triangle in either direction by pressing "j" or "k"
-        #region Rotation
-        float spincValue = _spinAction.ReadValue<float>() * _spinSpeed * Time.deltaTime;
-        transform.Rotate(0, 0, spincValue);
-        #endregion
+
+        void Update()
+        {
+            #region movement
+            Vector2 moveDirection = _moveAction.ReadValue<Vector2>();
+            Vector2 translation = moveDirection.normalized * _movementSpeed * Time.deltaTime;
+            transform.Translate(translation, Space.World);
+            #endregion
+
+            #region rotation
+            //exercise: give the player the ability to spin the triangle in 
+            // either direction by pressing “j” or “k”
+
+            float spinValue = _spinAction.ReadValue<float>() * _spinSpeed * Time.deltaTime;
+            transform.Rotate(0, 0, spinValue);
+
+            // Clamp rotation
+            Vector3 euler = transform.eulerAngles;
+
+            // Convert to signed range (-180 to 180)
+            if (euler.z > 180f)
+            {
+                euler.z -= 360f;
+            }
+            // Clamp, then assign back
+            euler.z = Mathf.Clamp(euler.z, _maxRotation, _minRotation);
+            transform.eulerAngles = euler;
+            #endregion
+
+            #region scaling (grow and shrink)
+            /* THIS WAS TO TRY OUT SCALING, BUT IT DOESN'T REALLY FIT IN THE GAME */
+            // float scaleValue = _scaleAction.ReadValue<float>() * _scaleSpeed * Time.deltaTime;
+            // Vector3 scaleChange = new Vector3(scaleValue, scaleValue, scaleValue);
+            // transform.localScale += scaleChange;
+
+            // //prevent the transform's scale from becoming negative
+            // Vector3 scale = transform.localScale;
+            // if (scale.x < 0)
+            // {
+            //     scale.x = 0;
+            // }
+            // if (scale.y < 0)
+            // {
+            //     scale.y = 0;
+            // }
+            // if (scale.z < 0)
+            // {
+            //     scale.z = 0;
+            // }
+            // transform.localScale = scale;
+            #endregion
+        }
+
+        private void FireButtonPressed(InputAction.CallbackContext context)
+        {
+            /*
+                To instantiate a prefab:
+                    1. create the prefab in the project directory
+                    2. give your script a way to reference the prefab
+                    3. call the Instantiate method
+            */
+            GameObject theProjectileThatIJustInstantiated =
+                Instantiate(_projectilePrefab, transform.position, Quaternion.identity);
+
+            Projectile projectileScript
+                = theProjectileThatIJustInstantiated.GetComponent<Projectile>();
+
+            //"transform.up" means "up according to the gameobject to which this script is attached"
+            //in other words, it means "up according to the ship"
+            projectileScript.Initialize(transform.up, _projectileSpeed);
+   
+            
+        }
+        private void FireButtonReleased(InputAction.CallbackContext context)
+        {
+            //Debug.Log("Space bar released yippee!");
+        }
     }
 }
